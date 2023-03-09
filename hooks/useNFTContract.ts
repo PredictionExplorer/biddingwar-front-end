e870c39302a162d5a95e9e0ed0cc6349e87d2d0b
Ionel <ionel2988@gmail.com>
2023-03-09 12:07:44 -0500

    Fix types issue

--- hooks/useNFTContract.ts
+++ hooks/useNFTContract.ts
@@ -1,8 +1,7 @@
 import { NFT_ADDRESS } from "../config/app";
 import NFT_ABI from "../contracts/NFT.json";
-import type { NFT } from "../contracts/types";
 import useContract from "./useContract";
 
 export default function useNFTContract() {
-  return useContract<NFT>(NFT_ADDRESS, NFT_ABI);
+  return useContract(NFT_ADDRESS, NFT_ABI);
 }
