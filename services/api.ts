ccf2ba281b768dde0d803c7d4a574a244f0c34e0
Ionel <ionel2988@gmail.com>
2023-03-31 09:04:35 -0400

    Fix issues

--- services/api.ts
+++ services/api.ts
@@ -21,6 +21,36 @@ class ApiService {
     const { data } = await axios.get(baseUrl + "token_info/" + token_id);
     return data;
   }
+
+  public async get(token_id: number | string) {
+    const { data } = await axios.get(baseUrl + "tokens/" + token_id);
+    const url = `http://198.58.105.159:9094/api/rwalk/tokens/history/${token_id}/0x895a6F444BE4ba9d124F61DF736605792B35D66b/0/1000`;
+    const res = await axios.get(url);
+    if (data) {
+      data.tokenHistory = res?.data?.TokenHistory;
+    }
+    return data;
+  }
+  
+  public async get_sell(id = -1) {
+    let { data } = await axios.get(baseUrl + "sell_offer");
+    data = data.sort((a: any, b: any) => a.Price - b.Price);
+    if (id == -1) return data;
+    const result = data.filter((x) => {
+      return x.TokenId == id;
+    });
+    return result;
+  }
+
+  public async get_buy(id = -1) {
+    let { data } = await axios.get(baseUrl + "buy_offer");
+    data = data.sort((a: any, b: any) => a.Price - b.Price);
+    if (id == -1) return data;
+    const result = data.filter((x) => {
+      return x.TokenId == id;
+    });
+    return result;
+  }
 }
 
 export default new ApiService();
