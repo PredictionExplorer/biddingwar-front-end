0c5c914b90da691296dd71c73764b6fbb2bbabb1
Ionel <ionel2988@gmail.com>
2023-05-11 22:12:07 -0400

    fix issues

--- components/NFT.tsx
+++ components/NFT.tsx
@@ -5,17 +5,19 @@ import { NFTImage, NFTSkeleton, NFTInfoWrapper, StyledCard } from "./styled";
 import router from "next/router";
 
 const NFT = ({ nft }) => {
+  const fileName = nft.TokenId.toString().padStart(6, "0");
+  const image = `https://cosmic-game.s3.us-east-2.amazonaws.com/${fileName}.png`;
   return (
     <StyledCard>
-      <CardActionArea onClick={() => router.push(`/detail/${nft.id}`)}>
+      <CardActionArea onClick={() => router.push(`/detail/${nft.TokenId}`)}>
         {!nft ? (
           <NFTSkeleton animation="wave" variant="rectangular" />
         ) : (
-          <NFTImage image={nft.image} />
+          <NFTImage image={image} />
         )}
         {nft && (
           <NFTInfoWrapper>
-            <Typography variant="caption">{formatId(nft.id)}</Typography>
+            <Typography variant="caption">{formatId(nft.TokenId)}</Typography>
           </NFTInfoWrapper>
         )}
       </CardActionArea>
