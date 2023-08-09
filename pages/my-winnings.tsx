c15fb8128af6f530d8700ea2c3006d572163a524
Ionel <ionel2988@gmail.com>
2023-08-09 16:25:17 -0400

    update my-winnings page

--- pages/my-winnings.tsx
+++ pages/my-winnings.tsx
@@ -3,6 +3,7 @@ import React, { useState } from "react";
 import {
   Box,
   Button,
+  Grid,
   Pagination,
   Table,
   TableBody,
@@ -21,6 +22,9 @@ import {
   TablePrimaryRow,
 } from "../components/styled";
 import { convertTimestampToDateTime } from "../utils";
+import DonatedNFT from "../components/DonatedNFT";
+import NFT from "../components/NFT";
+import api from "../services/api";
 
 const MyWinningsRow = ({ winning, handleETHClaim }) => {
   if (!winning) {
@@ -75,11 +79,25 @@ const MyWinningsTable = ({ list, handleETHClaim }) => {
   );
 };
 
-const MyWinnings = ({ list }) => {
+const MyWinnings = ({
+  list,
+  RaffleNFTs,
+  DonatedNFTs,
+  RWLKHolderNFTs,
+  CSHolderNFTs,
+}) => {
   const perPage = 20;
   const [curPage, setCurPage] = useState(1);
-  const claimAllETH = async () => {};
   const handleETHClaim = async (roundNum) => {};
+  const handleAllETHClaim = async () => {};
+  const handleRaffleNFTClaim = async (nftId) => {};
+  const handleAllRaffleNFTsClaim = async () => {};
+  const handleDonatedNFTClaim = async (nftId) => {};
+  const handleAllDonatedNFTsClaim = async () => {};
+  const handleRWLKHolderNFTClaim = async (nftId) => {};
+  const handleAllRWLKHolderNFTsClaim = async () => {};
+  const handleCSHolderNFTClaim = async (nftId) => {};
+  const handleAllCSHolderNFTsClaim = async () => {};
   return (
     <>
       <Head>
@@ -98,7 +116,7 @@ const MyWinnings = ({ list }) => {
         <Box mt={6}>
           <Box sx={{ display: "flex", justifyContent: "space-between", mb: 2 }}>
             <Typography variant="h5">Raffle ETH</Typography>
-            <Button onClick={claimAllETH} variant="contained">
+            <Button onClick={handleAllETHClaim} variant="contained">
               Claim All
             </Button>
           </Box>
@@ -117,34 +135,86 @@ const MyWinnings = ({ list }) => {
         <Box mt={6}>
           <Box sx={{ display: "flex", justifyContent: "space-between", mb: 2 }}>
             <Typography variant="h5">Raffle NFTs</Typography>
-            <Button onClick={claimAllETH} variant="contained">
+            <Button onClick={handleAllRaffleNFTsClaim} variant="contained">
               Claim All
             </Button>
           </Box>
+          <Grid container spacing={2} mt={2}>
+            {RaffleNFTs.map((nft, i) => (
+              <Grid
+                key={i}
+                sx={{ position: "relative" }}
+                item
+                xs={12}
+                sm={6}
+                md={3}
+                lg={3}
+              >
+                <NFT nft={nft} />
+              </Grid>
+            ))}
+          </Grid>
         </Box>
         <Box mt={6}>
           <Box sx={{ display: "flex", justifyContent: "space-between", mb: 2 }}>
             <Typography variant="h5">Donated NFTs</Typography>
-            <Button onClick={claimAllETH} variant="contained">
+            <Button onClick={handleAllDonatedNFTsClaim} variant="contained">
               Claim All
             </Button>
           </Box>
+          <Grid container spacing={2} mt={2}>
+            {DonatedNFTs.map((nft) => (
+              <Grid item key={nft.RecordId} xs={12} sm={6} md={3} lg={3}>
+                <DonatedNFT nft={nft} />
+              </Grid>
+            ))}
+          </Grid>
         </Box>
         <Box mt={6}>
           <Box sx={{ display: "flex", justifyContent: "space-between", mb: 2 }}>
             <Typography variant="h5">RWLK Holder Winning NFTs</Typography>
-            <Button onClick={claimAllETH} variant="contained">
+            <Button onClick={handleAllRWLKHolderNFTsClaim} variant="contained">
               Claim All
             </Button>
           </Box>
+          <Grid container spacing={2} mt={2}>
+            {RWLKHolderNFTs.map((nft, i) => (
+              <Grid
+                key={i}
+                sx={{ position: "relative" }}
+                item
+                xs={12}
+                sm={6}
+                md={3}
+                lg={3}
+              >
+                <NFT nft={nft} />
+              </Grid>
+            ))}
+          </Grid>
         </Box>
         <Box mt={6}>
           <Box sx={{ display: "flex", justifyContent: "space-between", mb: 2 }}>
             <Typography variant="h5">CS Holder Winning NFTs</Typography>
-            <Button onClick={claimAllETH} variant="contained">
+            <Button onClick={handleAllCSHolderNFTsClaim} variant="contained">
               Claim All
             </Button>
           </Box>
+          <Grid container spacing={2} mt={2}>
+            {CSHolderNFTs.map((nft, i) => (
+              <Grid
+                key={i}
+                sx={{ position: "relative" }}
+                item
+                xs={12}
+                sm={6}
+                md={3}
+                lg={3}
+              >
+                <NFT nft={nft} />
+              </Grid>
+            ))}
+          </Grid>
         </Box>
       </MainWrapper>
     </>
@@ -152,8 +222,16 @@ const MyWinnings = ({ list }) => {
 };
 
 export async function getServerSideProps() {
+  const nfts = await api.get_cst_list();
+  const nftDonations = await api.get_donations_nft_by_round(3);
   return {
-    props: { list: [] },
+    props: {
+      list: [],
+      RaffleNFTs: nfts.slice(6, 9),
+      DonatedNFTs: nftDonations,
+      RWLKHolderNFTs: nfts.slice(0, 3),
+      CSHolderNFTs: nfts.slice(3, 6),
+    },
   };
 }
 
