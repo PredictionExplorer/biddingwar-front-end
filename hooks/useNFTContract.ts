3017175bd739ab6642612b62215c374500605c56
Ionel <ionel2988@gmail.com>
2023-08-10 09:16:59 -0400

    fix issue

--- hooks/useNFTContract.ts
+++
@@ -1,7 +0,0 @@
-import NFT_ABI from '../contracts/NFT.json'
-
-import useContract from './useContract'
-
-export default function useNFTContract(address) {
-  return useContract(address, NFT_ABI)
-}
