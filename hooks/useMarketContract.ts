dc2173a8da3c558117bc27b18014dd71e720d4cd
Ionel <ionel2988@gmail.com>
2023-08-10 09:14:10 -0400

    fix issues

--- hooks/useMarketContract.ts
+++
@@ -1,8 +0,0 @@
-import { MARKET_ADDRESS } from '../config/app'
-import MARKET_ABI from '../contracts/Market.json'
-
-import useContract from './useContract'
-
-export default function useMarketContract() {
-  return useContract(MARKET_ADDRESS, MARKET_ABI)
-}
