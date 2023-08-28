b0fc919b9c31ac7e8eebf6bdc9a7ad96bbaa66d5
Ionel <ionel2988@gmail.com>
2023-08-28 11:38:06 -0400

    fix issues

--- services/api.ts
+++ services/api.ts
@@ -51,11 +51,6 @@ class ApiService {
     return data.NFTDonations;
   }
 
-  public async get_donation_nft_info(index: number) {
-    const { data } = await axios.get(cosmicGameBaseUrl + "donations/nft/info/" + index);
-    return data.NFTDonation;
-  }
-
   public async get_prize_list() {
     const { data } = await axios.get(cosmicGameBaseUrl + "prize/list/0/1000000");
     return data.PrizeClaims;
@@ -89,19 +84,9 @@ class ApiService {
     return data;
   }
 
-  public async get_raffle_deposits_by_round(round: number) {
-    const { data } = await axios.get(cosmicGameBaseUrl + `raffle/deposits/by_round/${round}`);
-    return data.RaffleDeposits;
-  }
-
   public async get_raffle_deposits_by_user(address: string) {
     const { data } = await axios.get(cosmicGameBaseUrl + `user/raffle_deposits/${address}`);
     return data.UserRaffleDeposits;
-  }
-
-  public async get_raffle_nft_winners_by_round(round: number) {
-    const { data } = await axios.get(cosmicGameBaseUrl + `raffle/nft_winners/by_round/${round}`);
-    return data.RaffleNFTWinners;
   }
 
   public async get_raffle_nft_winners_by_user(address: string) {
@@ -113,38 +98,6 @@ class ApiService {
     const { data } = await axios.get(baseUrl + "token_info/" + token_id);
     return data;
   }
-
-  public async get(token_id: number | string) {
-    const { data } = await axios.get(baseUrl + "tokens/" + token_id);
-    const url = `http://198.58.105.159:9094/api/rwalk/tokens/history/${token_id}/0x895a6F444BE4ba9d124F61DF736605792B35D66b/0/1000`;
-    const res = await axios.get(url);
-    if (data) {
-      data.tokenHistory = res?.data?.TokenHistory;
-    }
-    return data;
-  }
-
-  public async get_sell(id = -1) {
-    let { data } = await axios.get(baseUrl + "sell_offer");
-    data = data.sort((a: any, b: any) => a.Price - b.Price);
-    if (id == -1) return data;
-    const result = data.filter((x) => {
-      return x.TokenId == id;
-    });
-    return result;
-  }
-
-  public async get_buy(id = -1) {
-    let { data } = await axios.get(baseUrl + "buy_offer");
-    data = data.sort((a: any, b: any) => a.Price - b.Price);
-    if (id == -1) return data;
-    const result = data.filter((x) => {
-      return x.TokenId == id;
-    });
-    return result;
-  }
 }
 
 export default new ApiService();
-
-// claim prize page nft
